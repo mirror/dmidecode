414f026d888d091813cda55ab54496f4642b4bbd
Jean Delvare <jdelvare@suse.de>
2004-05-02 17:08:48 +0000

    New mem_chunk function. It returns a buffer containing a copy of a given chunk of the physical memory.

--- util.c
+++ util.c
@@ -2,7 +2,7 @@
  * Common "util" functions
  * This file is part of the dmidecode project.
  *
- *   (C) 2002-2003 Jean Delvare <khali@linux-fr>
+ *   (C) 2002-2004 Jean Delvare <khali@linux-fr>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -26,8 +26,15 @@
  *
  */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#ifdef USE_MMAP
+#include <sys/mman.h>
+#endif /* USE MMAP */
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 
 #include "types.h"
@@ -72,4 +79,66 @@ int checksum(const u8 *buf, size_t len)
 	for(a=0; a<len; a++)
 		sum+=buf[a];
 	return (sum==0);
+}
+
+/*
+ * Copy a physical memory chunk into a memory buffer.
+ * This function allocates memory.
+ */
+void *mem_chunk(off_t base, off_t len, const char *devmem)
+{
+	void *p;
+	int fd;
+#ifdef USE_MMAP
+	size_t mmoffset;
+	void *mmp;
+#endif
+	
+	if((fd=open(devmem, O_RDONLY))==-1)
+	{
+		perror(devmem);
+		return NULL;
+	}
+	
+	if((p=malloc(len))==NULL)
+		return NULL;
+	
+#ifdef USE_MMAP
+	mmoffset=base%getpagesize();
+	/*
+	 * Please note that we don't use mmap() for performance reasons here,
+	 * but to workaround problems many people encountered when trying
+	 * to read from /dev/mem using regular read() calls.
+	 */
+	mmp=mmap(0, mmoffset+len, PROT_READ, MAP_PRIVATE, fd, base-mmoffset);
+	if(mmp==MAP_FAILED)
+	{
+		perror(devmem);
+		free(p);
+		return NULL;
+	}
+	
+	memcpy(p, (u8 *)mmp+mmoffset, len);
+	
+	if(munmap(mmp, mmoffset+0x20)==-1)
+		perror(devmem);
+#else /* USE_MMAP */
+	if(lseek(fd, base, SEEK_SET)==-1)
+	{
+		perror(devmem);
+		free(p);
+		return NULL;
+	}
+	
+	if(myread(fd, p, len, devmem)==-1)
+	{
+		free(p);
+		return NULL;
+	}
+#endif /* USE_MMAP */
+	
+	if(close(fd)==-1)
+		perror(devmem);
+	
+	return p;
 }
--- util.h
+++ util.h
@@ -4,3 +4,4 @@
 
 int myread(int fd, u8 *buf, size_t count, const char *prefix);
 int checksum(const u8 *buf, size_t len);
+void *mem_chunk(off_t base, off_t len, const char *devmem);
