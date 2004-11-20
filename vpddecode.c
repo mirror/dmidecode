ae3ff43e2fc59fa4538d605379112409abd23b04
Jean Delvare <jdelvare@suse.de>
2004-11-20 20:44:06 +0000

    Discard useless includes.

--- vpddecode.c
+++ vpddecode.c
@@ -33,12 +33,8 @@
  *    additional lookup table for the product name.
  */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 
 #include "version.h"
