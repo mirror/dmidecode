mirror/dmidecode

commit 3f70b3515d91e2eea0d9a1a36aef6eefc9ec513c

dmidecode: Fix up invalid DMI type 34 structure length
Several boards have a bug where some type 34 structures have their
length incorrectly set to 0x10 instead of 0x0B. This causes the first
5 characters of the device name to be trimmed. It's easy to check and
fix, so do it, but warn.

dmidecode.c
@@ -2928,6 +2928,29 @@ static void dmi_64bit_memory_error_address(u64 code)
  * 7.35 Management Device (Type 34)
  */
 
+/*
+ * Several boards have a bug where some type 34 structures have their
+ * length incorrectly set to 0x10 instead of 0x0B. This causes the
+ * first 5 characters of the device name to be trimmed. It's easy to
+ * check and fix, so do it, but warn.
+ */
+static void dmi_fixup_type_34(struct dmi_header *h)
+{
+	u8 *p = h->data;
+	int i;
+
+ 	if (h->length != 0x10)
+		return;
+
+ 	/* Make sure the hidden data is ASCII only */
+	for (i = 0x0B; i < 0x10; i++)
+		if (p[i] < 32 || p[i] >= 127)
+			return;
+
+	printf("Invalid entry length (%u). Fixed up to %u.\n", 0x10, 0x0B);
+	h->length = 0x0B;
+}
+
 static const char *dmi_management_device_type(u8 code)
 {
 	/* 7.35.1 */
@@ -4409,6 +4432,10 @@ static void dmi_table_decode(u8 *buf, u32 len, u16 num, u16 ver, u32 flags)
 		/* assign vendor for vendor-specific decodes later */
 		if (h.type == 1 && h.length >= 5)
 			dmi_set_vendor(dmi_string(&h, data[0x04]));
+
+		/* Fixup a common mistake */
+		if (h.type == 34)
+			dmi_fixup_type_34(&h);
 
 		/* look for the next handle */
 		next = data + h.length;

commit cff11afa886a0147d734b650755d232b5e7f2099

dmidecode: Hide irrelevant fixup message
Only display the message about type 34 length fixup if the entry in
question is going to be displayed. Otherwise it's only confusing.

This fixes bug #109024:
http://savannah.nongnu.org/support/?109024

Fixes: 3f70b3515d91 ("dmidecode: Fix up invalid DMI type 34 structure length")

dmidecode.c
@@ -2949,7 +2949,7 @@ static void dmi_64bit_memory_error_address(u64 code)
  * first 5 characters of the device name to be trimmed. It's easy to
  * check and fix, so do it, but warn.
  */
-static void dmi_fixup_type_34(struct dmi_header *h)
+static void dmi_fixup_type_34(struct dmi_header *h, int display)
 {
 	u8 *p = h->data;
 
@@ -2957,7 +2957,9 @@ static void dmi_fixup_type_34(struct dmi_header *h)
 	if (h->length == 0x10
 	 && is_printable(p + 0x0B, 0x10 - 0x0B))
 	{
-		printf("Invalid entry length (%u). Fixed up to %u.\n", 0x10, 0x0B);
+		if (!(opt.flags & FLAG_QUIET) && display)
+			printf("Invalid entry length (%u). Fixed up to %u.\n",
+				0x10, 0x0B);
 		h->length = 0x0B;
 	}
 }
@@ -4446,7 +4448,7 @@ static void dmi_table_decode(u8 *buf, u32 len, u16 num, u16 ver, u32 flags)
 
 		/* Fixup a common mistake */
 		if (h.type == 34)
-			dmi_fixup_type_34(&h);
+			dmi_fixup_type_34(&h, display);
 
 		/* look for the next handle */
 		next = data + h.length;