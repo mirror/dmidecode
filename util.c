mirror/dmidecode

commit 2339e722b49efda38bdfe4a4b9996efc440c5567

util: Add read_file() function for reading sysfs files
Add a function that can read a complete, unknown size file for
reading entry point files from sysfs.  This function models its signature
on the mem_chunk() funtion, so it also allocates memory that the caller
needs to free.  The files that we are interested in reading are very small,
and have a known upper bound on the size.  The EINTR handling is based
on the myread() function.

Contributed by Roy Franz.

util.c
@@ -89,6 +89,61 @@ int checksum(const u8 *buf, size_t len)
 }
 
 /*
+ * Reads all of file, up to max_len bytes.
+ * A buffer of max_len bytes is allocated by this function, and
+ * needs to be freed by the caller.
+ * This provides a similar usage model to mem_chunk()
+ *
+ * Returns pointer to buffer of max_len bytes, or NULL on error
+ *
+ */
+void *read_file(size_t max_len, const char *filename)
+{
+	int fd;
+	size_t r2 = 0;
+	ssize_t r;
+	u8 *p;
+
+	/*
+	 * Don't print error message on missing file, as we will try to read
+	 * files that may or may not be present.
+	 */
+	if ((fd = open(filename, O_RDONLY)) == -1)
+	{
+		if (errno != ENOENT)
+			perror(filename);
+		return(NULL);
+	}
+
+	if ((p = malloc(max_len)) == NULL)
+	{
+		perror("malloc");
+		return NULL;
+	}
+
+	do
+	{
+		r = read(fd, p + r2, max_len - r2);
+		if (r == -1)
+		{
+			if (errno != EINTR)
+			{
+				close(fd);
+				perror(filename);
+				free(p);
+				return NULL;
+			}
+		}
+		else
+			r2 += r;
+	}
+	while (r != 0);
+
+	close(fd);
+	return p;
+}
+
+/*
  * Copy a physical memory chunk into a memory buffer.
  * This function allocates memory.
  */

util.h
@@ -25,6 +25,7 @@
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
 
 int checksum(const u8 *buf, size_t len);
+void *read_file(size_t len, const char *filename);
 void *mem_chunk(size_t base, size_t len, const char *devmem);
 int write_dump(size_t base, size_t len, const void *data, const char *dumpfile, int add);
 u64 u64_range(u64 start, u64 end);

commit 6953b627a0f11f70662496a77b67aefa9dc40968

util: Don't leak a file descriptor in read_file
If memory allocation fails, we should close the file descriptor
before returning the error.

Also remove unneeded parentheses on return, return isn't a function.

Signed-off-by: Jean Delvare <jdelvare@suse.de>
Fixes: 2339e722b49e ("util: Add read_file() function for reading sysfs files")

util.c
@@ -113,13 +113,13 @@ void *read_file(size_t *max_len, const char *filename)
 	{
 		if (errno != ENOENT)
 			perror(filename);
-		return(NULL);
+		return NULL;
 	}
 
 	if ((p = malloc(*max_len)) == NULL)
 	{
 		perror("malloc");
-		return NULL;
+		goto out;
 	}
 
 	do
@@ -129,10 +129,10 @@ void *read_file(size_t *max_len, const char *filename)
 		{
 			if (errno != EINTR)
 			{
-				close(fd);
 				perror(filename);
 				free(p);
-				return NULL;
+				p = NULL;
+				goto out;
 			}
 		}
 		else
@@ -140,8 +140,9 @@ void *read_file(size_t *max_len, const char *filename)
 	}
 	while (r != 0);
 
-	close(fd);
 	*max_len = r2;
+out:
+	close(fd);
 
 	return p;
 }