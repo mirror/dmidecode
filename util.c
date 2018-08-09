mirror/dmidecode

commit 458f73d58c24a7addce82bf1e8bfb8c2554ca458

Fix error paths in mem_chunk
Use a common error path in function mem_chunk, to make sure it does
not leak memory and does not leave an opened file descriptor behind,
without duplicating the cleaning code.

util.c
@@ -166,7 +166,7 @@ void *mem_chunk(off_t base, size_t len, const char *devmem)
 	if ((p = malloc(len)) == NULL)
 	{
 		perror("malloc");
-		return NULL;
+		goto out;
 	}
 
 #ifdef USE_MMAP
@@ -174,8 +174,7 @@ void *mem_chunk(off_t base, size_t len, const char *devmem)
 	{
 		fprintf(stderr, "%s: ", devmem);
 		perror("stat");
-		free(p);
-		return NULL;
+		goto err_free;
 	}
 
 	/*
@@ -186,8 +185,7 @@ void *mem_chunk(off_t base, size_t len, const char *devmem)
 	{
 		fprintf(stderr, "mmap: Can't map beyond end of file %s\n",
 			devmem);
-		free(p);
-		return NULL;
+		goto err_free;
 	}
 
 #ifdef _SC_PAGESIZE
@@ -220,19 +218,17 @@ try_read:
 	{
 		fprintf(stderr, "%s: ", devmem);
 		perror("lseek");
-		free(p);
-		return NULL;
+		goto err_free;
 	}
 
-	if (myread(fd, p, len, devmem) == -1)
-	{
-		free(p);
-		return NULL;
-	}
+	if (myread(fd, p, len, devmem) == 0)
+		goto out;
 
-#ifdef USE_MMAP
+err_free:
+	free(p);
+	p = NULL;
+
 out:
-#endif
 	if (close(fd) == -1)
 		perror(devmem);
 

commit 2818946fc800aaefa7fd25d52e45babd6db13d9b

util: Don't close the same file descriptor twice
If myread() fails, it closes the file descriptor it was reading from.
On success, it does not. The caller is always closing the file
descriptor afterward, which results in double close in the error
case. This causes an additional error:

/dev/mem: Bad file descriptor

As the caller is always taking care of closing the file descriptor,
there is no need for myread() to do it.

Signed-off-by: Jean Delvare <jdelvare@suse.de>
Fixes: 458f73d58c24 ("Fix error paths in mem_chunk")

util.c
@@ -59,7 +59,6 @@ static int myread(int fd, u8 *buf, size_t count, const char *prefix)
 		{
 			if (errno != EINTR)
 			{
-				close(fd);
 				perror(prefix);
 				return -1;
 			}
@@ -70,7 +69,6 @@ static int myread(int fd, u8 *buf, size_t count, const char *prefix)
 
 	if (r2 != count)
 	{
-		close(fd);
 		fprintf(stderr, "%s: Unexpected end of file\n", prefix);
 		return -1;
 	}