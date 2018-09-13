mirror/dmidecode

commit bd78a5dfd47043db08982464bcae4759c0980975

dmidecode: Don't allocate more memory than needed
If the actual DMI table size is less than the announced maximum
(which is allowed for 64-bit SMBIOS 3 entry points), we may allocate
significantly more memory than is actually needed. If reading from
/dev/mem, there's nothing we can do about that. However, is reading
from sysfs or from a dump file, we can easily check the file size
and compute the actual table size from it. That way we only allocate
the required amount of memory.

Credits to Lionel Debroux for seeding the idea when performing fuzz
testing on dmidecode.

Signed-off-by: Jean Delvare <jdelvare@suse.de>

util.c
@@ -90,16 +90,16 @@ int checksum(const u8 *buf, size_t len)
 
 /*
  * Reads all of file from given offset, up to max_len bytes.
- * A buffer of max_len bytes is allocated by this function, and
+ * A buffer of at most max_len bytes is allocated by this function, and
  * needs to be freed by the caller.
  * This provides a similar usage model to mem_chunk()
  *
- * Returns pointer to buffer of max_len bytes, or NULL on error, and
+ * Returns a pointer to the allocated buffer, or NULL on error, and
  * sets max_len to the length actually read.
- *
  */
 void *read_file(off_t base, size_t *max_len, const char *filename)
 {
+	struct stat statbuf;
 	int fd;
 	size_t r2 = 0;
 	ssize_t r;
@@ -122,6 +122,15 @@ void *read_file(off_t base, size_t *max_len, const char *filename)
 		perror("lseek");
 		p = NULL;
 		goto out;
+	}
+
+	/*
+	 * Check file size, don't allocate more than can be read.
+	 */
+	if (fstat(fd, &statbuf) == 0)
+	{
+		if (base + (off_t)*max_len > statbuf.st_size)
+			*max_len = statbuf.st_size - base;
 	}
 
 	if ((p = malloc(*max_len)) == NULL)

commit 95c712fe7a58c98b85165f11a6afb83562a14fd7

dmidecode: Sanity check the table offset in dump files
If the offset (base) is beyond the end of the file (statbuf.st_size),
the computations will lead to an integer overflow. As it doesn't make
sense in the first place, check for this condition and fail
immediately.

This bug was discovered by Lionel Debroux using the AFL fuzzer and
AddressSanitizer.

Signed-off-by: Jean Delvare <jdelvare@suse.de>
Fixes: bd78a5dfd470 ("dmidecode: Don't allocate more memory than needed")

util.c
@@ -2,7 +2,7 @@
  * Common "util" functions
  * This file is part of the dmidecode project.
  *
- *   Copyright (C) 2002-2017 Jean Delvare <jdelvare@suse.de>
+ *   Copyright (C) 2002-2018 Jean Delvare <jdelvare@suse.de>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -117,7 +117,14 @@ void *read_file(off_t base, size_t *max_len, const char *filename)
 	 */
 	if (fstat(fd, &statbuf) == 0)
 	{
-		if (base + (off_t)*max_len > statbuf.st_size)
+		if (base >= statbuf.st_size)
+		{
+			fprintf(stderr, "%s: Can't read data beyond EOF\n",
+				filename);
+			p = NULL;
+			goto out;
+		}
+		if (*max_len > (size_t)statbuf.st_size - base)
 			*max_len = statbuf.st_size - base;
 	}
 