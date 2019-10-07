9bce894b7730013120d4c71f964d4cf8402cb956
Erwan Velu <e.velu@criteo.com>
2019-10-07 15:36:47 +0200

    dmidecode: Adding bios-revision to -s option
    Some hardware vendors like HPe use the Version field to store the
    bios generation like (U30, U32, A40, ...).  If you want to get the
    "release" version of this bios generation, the bios revision field
    must be considered.
    
    A typical output of this kind of server looks like :
    
              BIOS Information
                      Vendor: HPE
                      Version: A40
                      Release Date: 07/20/2019
                      [...]
                      BIOS Revision: 2.0
                      Firmware Revision: 1.45
    
    Add a "bios-revision" entry into the '-s' option.
    
    A typical usage of this feature looks like :
    
    	[root@host] dmidecode -s bios-revision
    	2.0
    
    Signed-off-by: Erwan Velu <e.velu@criteo.com>
    Signed-off-by: Jean Delvare <jdelvare@suse.de>

--- dmiopt.c
+++ dmiopt.c
@@ -151,6 +151,7 @@ static const struct string_keyword opt_string_keyword[] = {
 	{ "bios-vendor", 0, 0x04 },
 	{ "bios-version", 0, 0x05 },
 	{ "bios-release-date", 0, 0x08 },
+	{ "bios-revision", 0, 0x15 },		/* 0x14 and 0x15 */
 	{ "system-manufacturer", 1, 0x04 },
 	{ "system-product-name", 1, 0x05 },
 	{ "system-version", 1, 0x06 },
