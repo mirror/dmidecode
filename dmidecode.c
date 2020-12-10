mirror/dmidecode

commit b289de9deb325eeef7919a1f4e3ac1f93b099c16

Add new processor characteristics bits from SMBIOS spec 3.4.0
* The bit to indicate 128-bit capable processor
* The bit to indicate processor supports returning ARM64  SoC ID

dmidecode.c
@@ -1388,7 +1388,9 @@ static void dmi_processor_characteristics(const char *attr, u16 code)
 		"Hardware Thread",
 		"Execute Protection",
 		"Enhanced Virtualization",
-		"Power/Performance Control" /* 7 */
+		"Power/Performance Control",
+		"128-bit Capable"
+		"Arm64 SoC ID" /* 9 */
 	};
 
 	if ((code & 0x00FC) == 0)
@@ -1398,7 +1400,7 @@ static void dmi_processor_characteristics(const char *attr, u16 code)
 		int i;
 
 		pr_list_start(attr, NULL);
-		for (i = 2; i <= 7; i++)
+		for (i = 2; i <= 9; i++)
 			if (code & (1 << i))
 				pr_list_item("%s", characteristics[i - 2]);
 		pr_list_end();

commit ac6a64496fc99a5cc05ebe4dd7d1b88f60041009

Missing comma in the dmi_slot_type()
In the initialization of "type", a suspicious concatenated string
"PCI Express Mini 76-pinPCI Express 4 SFF-8639 (U.2)" is produced
due to a missing comma between lines.

[JD: Fixed a similar bug in dmi_processor_characteristics.]

Fixes: b289de9deb32 ("Add new processor characteristics bits from SMBIOS spec 3.4.0")
Fixes: c54348130a2a ("dmidecode: Add new system slot types from SMBIOS spec 3.4.0")
Signed-off-by: Lianbo Jiang <lijiang@redhat.com>
Signed-off-by: Jean Delvare <jdelvare@suse.de>

dmidecode.c
@@ -1389,7 +1389,7 @@ static void dmi_processor_characteristics(const char *attr, u16 code)
 		"Execute Protection",
 		"Enhanced Virtualization",
 		"Power/Performance Control",
-		"128-bit Capable"
+		"128-bit Capable",
 		"Arm64 SoC ID" /* 9 */
 	};
 
@@ -1939,7 +1939,7 @@ static const char *dmi_slot_type(u8 code)
 		"PCI Express 3 SFF-8639 (U.2)",
 		"PCI Express Mini 52-pin with bottom-side keep-outs",
 		"PCI Express Mini 52-pin without bottom-side keep-outs",
-		"PCI Express Mini 76-pin"
+		"PCI Express Mini 76-pin",
 		"PCI Express 4 SFF-8639 (U.2)",
 		"PCI Express 5 SFF-8639 (U.2)",
 		"OCP NIC 3.0 Small Form Factor (SFF)",