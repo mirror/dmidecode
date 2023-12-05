mirror/dmidecode

commit 6fc53b3ececeb0c41eecf5ad8c272fe5fc08c69a

bash: add completions
Offer only long options, but recognize short ones, too, per rationale at
https://github.com/scop/bash-completion/blob/4d0bffb791c34c96114aeb2e4f6726b80aa8698e/CONTRIBUTING.md?plain=1#L136-L153

Signed-off-by: Ville Skyttä <ville.skytta@iki.fi>

completion/dmidecode.bash
@@ -1,0 +1,66 @@
+# bash completion for dmidecode                            -*- shell-script -*-
+
+_comp_cmd_dmidecode() {
+	local cur prev
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD - 1]}
+
+	case $prev in
+	-d | --dev-mem | --dump-bin | --from-dump)
+		if [[ $prev == -d || $prev == --dev-mem ]]; then
+			: "${cur:=/dev/}"
+		fi
+		local IFS=$'\n'
+		compopt -o filenames
+		COMPREPLY=($(compgen -f -- "$cur"))
+		return 0
+		;;
+	-s | --string)
+		COMPREPLY=($(compgen -W '$(
+			"$1" --string 2>&1 | while IFS=\$'\\n' read -r line ; do
+				[[ $line == "  "* ]] && printf "%s\n" "$line"
+			done
+		)' -- "$cur"))
+		return 0
+		;;
+	-t | --type)
+		COMPREPLY=($(compgen -W '$(
+			"$1" --type 2>&1 | while IFS=\$'\\n' read -r line ; do
+				[[ $line == "  "* ]] && printf "%s\n" "$line"
+			done
+		)' -- "$cur"))
+		return 0
+		;;
+	--dump-bin | --from-dump)
+		local IFS=$'\n'
+		compopt -o filenames
+		COMPREPLY=($(compgen -f -- "$cur"))
+		return 0
+		;;
+	-[hVH] | --help | --version | --handle | --oem-string)
+		return 0
+		;;
+	esac
+
+	if [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W '
+			--dev-mem
+			--help
+			--quiet
+			--string
+			--type
+			--handle
+			--dump
+			--dump-bin
+			--from-dump
+			--no-sysfs
+			--oem-string
+			--version
+		' -- "$cur"))
+		return 0
+	fi
+
+} && complete -F _comp_cmd_dmidecode dmidecode
+
+# ex: filetype=sh

Makefile
@@ -36,6 +36,7 @@ sbindir = $(prefix)/sbin
 mandir  = $(prefix)/share/man
 man8dir = $(mandir)/man8
 docdir  = $(prefix)/share/doc/dmidecode
+compdir = $(shell pkg-config --variable=completionsdir bash-completion 2>/dev/null || echo $(prefix)/etc/bash_completion.d)
 
 INSTALL         := install
 INSTALL_DATA    := $(INSTALL) -m 644
@@ -113,9 +114,9 @@ util.o : util.c types.h util.h config.h
 strip : $(PROGRAMS)
 	strip $(PROGRAMS)
 
-install : install-bin install-man install-doc
+install : install-bin install-man install-doc install-completion
 
-uninstall : uninstall-bin uninstall-man uninstall-doc
+uninstall : uninstall-bin uninstall-man uninstall-doc uninstall-completion
 
 install-bin : $(PROGRAMS)
 	$(INSTALL_DIR) $(DESTDIR)$(sbindir)
@@ -143,6 +144,19 @@ install-doc :
 
 uninstall-doc :
 	$(RM) -r $(DESTDIR)$(docdir)
+
+install-completion :
+	if [ -d $(compdir) ] ; then \
+	$(INSTALL_DIR) $(DESTDIR)$(compdir) ; \
+	for program in $(PROGRAMS) ; do \
+	$(INSTALL_DATA) completion/$$program.bash $(DESTDIR)$(compdir)/$$program ; done ; \
+	fi
+
+uninstall-completion :
+	if [ -d $(DESTDIR)$(compdir) ]; then \
+	for program in $(PROGRAMS) ; do \
+	$(RM) $(DESTDIR)$(compdir)/$$program ; done ; \
+	fi
 
 clean :
 	$(RM) *.o $(PROGRAMS) core

completion/biosdecode.bash
@@ -1,0 +1,40 @@
+# bash completion for biosdecode                           -*- shell-script -*-
+
+_comp_cmd_biosdecode() {
+	local cur prev
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD - 1]}
+
+	case $prev in
+	-d | --dev-mem)
+		: "${cur:=/dev/}"
+		local IFS=$'\n'
+		compopt -o filenames
+		COMPREPLY=($(compgen -f -- "$cur"))
+		return 0
+		;;
+	--pir)
+		COMPREPLY=($(compgen -W '
+			full
+		' -- "$cur"))
+		return 0
+		;;
+	-[hV] | --help | --version)
+		return 0
+		;;
+	esac
+
+	if [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W '
+			--dev-mem
+			--pir
+			--help
+			--version
+		' -- "$cur"))
+		return 0
+	fi
+
+} && complete -F _comp_cmd_biosdecode biosdecode
+
+# ex: filetype=sh

completion/ownership.bash
@@ -1,0 +1,33 @@
+# bash completion for ownership                            -*- shell-script -*-
+
+_comp_cmd_ownership() {
+	local cur prev
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD - 1]}
+
+	case $prev in
+	-d | --dev-mem)
+		: "${cur:=/dev/}"
+		local IFS=$'\n'
+		compopt -o filenames
+		COMPREPLY=($(compgen -f -- "$cur"))
+		return 0
+		;;
+	-[hV] | --help | --version)
+		return 0
+		;;
+	esac
+
+	if [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W '
+			--dev-mem
+			--help
+			--version
+		' -- "$cur"))
+		return 0
+	fi
+
+} && complete -F _comp_cmd_ownership ownership
+
+# ex: filetype=sh

completion/vpddecode.bash
@@ -1,0 +1,43 @@
+# bash completion for vpddecode                            -*- shell-script -*-
+
+_comp_cmd_vpddecode() {
+	local cur prev
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+	prev=${COMP_WORDS[COMP_CWORD - 1]}
+
+	case $prev in
+	-d | --dev-mem)
+		: "${cur:=/dev/}"
+		local IFS=$'\n'
+		compopt -o filenames
+		COMPREPLY=($(compgen -f -- "$cur"))
+		return 0
+		;;
+	-s | --string)
+		COMPREPLY=($(compgen -W '$(
+			"$1" --string 2>&1 | while IFS=\$'\\n' read -r line ; do
+				[[ $line == "  "* ]] && printf "%s\n" "$line"
+			done
+		' -- "$cur"))
+		return 0
+		;;
+	-[hV] | --help | --version)
+		return 0
+		;;
+	esac
+
+	if [[ $cur == -* ]]; then
+		COMPREPLY=($(compgen -W '
+			--dev-mem
+			--help
+			--string
+			--dump
+			--version
+		' -- "$cur"))
+		return 0
+	fi
+
+} && complete -F _comp_cmd_vpddecode vpddecode
+
+# ex: filetype=sh

commit e80d706976f30487447e00f58c9669cbbd902967

dmidecode completion: Use --list-strings and --list-types
Use the new options --list-strings and --list-types to simplify the
completion of --string and --type options, respectively. This also
fixes a bug (trailing "n" being stripped) as a side effect.

Signed-off-by: Jean Delvare <jdelvare@suse.de>
Fixes: 6fc53b3ecece ("bash: add completions")

completion/dmidecode.bash
@@ -17,19 +17,11 @@ _comp_cmd_dmidecode() {
 		return 0
 		;;
 	-s | --string)
-		COMPREPLY=($(compgen -W '$(
-			"$1" --string 2>&1 | while IFS=\$'\\n' read -r line ; do
-				[[ $line == "  "* ]] && printf "%s\n" "$line"
-			done
-		)' -- "$cur"))
+		COMPREPLY=($(compgen -W '$("$1" --list-strings)' -- "$cur"))
 		return 0
 		;;
 	-t | --type)
-		COMPREPLY=($(compgen -W '$(
-			"$1" --type 2>&1 | while IFS=\$'\\n' read -r line ; do
-				[[ $line == "  "* ]] && printf "%s\n" "$line"
-			done
-		)' -- "$cur"))
+		COMPREPLY=($(compgen -W '$("$1" --list-types)' -- "$cur"))
 		return 0
 		;;
 	--dump-bin | --from-dump)
@@ -49,7 +41,9 @@ _comp_cmd_dmidecode() {
 			--help
 			--quiet
 			--string
+			--list-strings
 			--type
+			--list-types
 			--handle
 			--dump
 			--dump-bin