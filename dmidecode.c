329005b0d796670f1eadb25f8bd2851ab1af1e03
Jean Delvare <jdelvare@suse.de>
2024-04-12 18:32:04 +0200

    Update copyright year

--- dmidecode.c
+++ dmidecode.c
@@ -2,7 +2,7 @@
  * DMI Decode
  *
  *   Copyright (C) 2000-2002 Alan Cox <alan@redhat.com>
- *   Copyright (C) 2002-2020 Jean Delvare <jdelvare@suse.de>
+ *   Copyright (C) 2002-2024 Jean Delvare <jdelvare@suse.de>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
